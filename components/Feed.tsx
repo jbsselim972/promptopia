cb313a431fa28cbc313e24681270cbff3234ea94
Jbsselim <jbsselim@gmail.com>
2023-05-30 14:37:03 -0400

    fetching all prompts in the Feed

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,7 +1,63 @@
-import React from "react";
+"use client";
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+import { IPrompt } from "@models/prompt";
+
+const PromptCardList = ({
+  data,
+  handleTagClick,
+}: {
+  data: IPrompt[];
+  handleTagClick: () => void;
+}) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((prompt) => (
+        <PromptCard
+          key={prompt._id}
+          prompt={prompt}
+          handleTagClick={handleTagClick}
+          handleDelete={() => {}}
+          handleEdit={() => {}}
+        />
+      ))}
+    </div>
+  );
+};
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState("");
+  const [prompts, setPrompts] = useState<IPrompt[]>([]);
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value);
+  };
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+
+      setPrompts(data);
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          className="search_input peer"
+          required
+        />
+      </form>
+
+      <PromptCardList data={prompts} handleTagClick={() => {}} />
+    </section>
+  );
 };
 
 export default Feed;

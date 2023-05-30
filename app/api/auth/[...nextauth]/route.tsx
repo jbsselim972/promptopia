9e8fdcb97d7b09ae50823e4c80b07c87b69fa78a
Jbsselim <jbsselim@gmail.com>
2023-05-30 07:46:52 -0400

    configuring next-auth route & callbacks

---
+++ app/api/auth/[...nextauth]/route.tsx
@@ -0,0 +1,46 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+import User from "@models/user";
+import { connectToDB } from "@utils/database";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    async session({ session }: any) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
+      });
+
+      session.user.id = sessionUser!._id.toString();
+      return session;
+    },
+    async signIn({ profile }: any) {
+      try {
+        await connectToDB();
+
+        //check if user already exist
+        const userExist = await User.findOne({ email: profile.email });
+        //if not create user
+        if (!userExist) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+        return true;
+      } catch (error) {
+        console.error(error);
+        return false;
+      }
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
